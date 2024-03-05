fb6e81cb1b7f2ea8c951bd560e9f0f14ef30ce41
Patryk Zdebik <38981197+pzdebik@users.noreply.github.com>
2024-03-05 19:22:32 +0100

    Compute final score

--- word-spitter.c
+++ word-spitter.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 // This array's length is equal to the English alphabet's length (0-25)
 const int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1,
                       1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 void printGameTitle();
+int computeScore(char word[]);
 
 int main(void)
 {
@@ -29,5 +32,24 @@ void printGameTitle()
                   "\\ \\    / / ___  _ _  __| |      / __|| '_ \\(_)| |_ | |_  ___  _ _ \n"
                   " \\ \\/\\/ / / _ \\| '_|/ _` |      \\__ \\| .__/| ||  _||  _|/ -_)| '_|\n"
                   "  \\_/\\_/  \\___/|_|  \\__/_|      |___/|_|   |_| \\__| \\__|\\___||_|  \n");
+}
+
+// Computes the final score using POINTS array
+int computeScore(char word[])
+{
+    int score = 0;
+
+    for (int i = 0, length = strlen(word); i < length; i++)
+    {
+        // Every character of the passed word is turned to uppercase
+        char upperedChar = toupper(word[i]);
+        // Final score is the result of summing operation on every point obtained from the subtraction operation
+        // Uppered char is subtracted from the first letter of the alphabet, which gives us the position of the letter in the alphabet
+        if (isalpha(upperedChar))
+        {
+            score += POINTS[upperedChar - 'A'];
+        }
+    }
+    return score;
 }
 
